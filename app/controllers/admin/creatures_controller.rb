bd19af76be421fe1afe83aae8ac249d22fd01e17
dannyradden <dradden@gmail.com>
2017-04-18 16:42:07 -0600

    Finish admin edit creature functionality

--- app/controllers/admin/creatures_controller.rb
+++ app/controllers/admin/creatures_controller.rb
@@ -2,4 +2,20 @@ class Admin::CreaturesController < Admin::BaseController
   def index
     @creatures = Creature.all
   end
+
+  def edit
+    @creature = Creature.find(params[:id])
+  end
+
+  def update
+    creature = Creature.find(params[:id])
+    creature.update(creature_params)
+
+    redirect_to admin_creatures_path
+  end
+
+  private
+  def creature_params
+    params.require(:creature).permit(:breed, :description, :price, :status, :image_url)
+  end
 end
--- spec/features/admin_edits_a_creature_spec.rb
+++ spec/features/admin_edits_a_creature_spec.rb
@@ -13,27 +13,31 @@ RSpec.feature 'Admin edits and creature' do
       click_on('Edit')
 
       expect(current_path).to eq(edit_admin_creature_path(creature))
-      within('.creature') do
-        expect(page).to have_field(creature[breed])
-        expect(page).to have_field(creature[description])
-        expect(page).to have_field(creature[image_url])
-        expect(page).to have_field(creature[status])
 
-        expect(page).to have_content(creature.breed)
-        expect(page).to have_content(creature.description)
-        expect(page).to have_css("img[src*='#{creature.id}?set']")
-        expect(page).to have_content(creature.status)
+      within('.creature') do
+        expect(page).to have_field('creature[breed]')
+        expect(page).to have_field('creature[description]')
+        expect(page).to have_field('creature[image_url]')
+        expect(page).to have_field('creature[status]')
+
+        expect(find_field('creature[breed]').value).to eq(creature.breed)
+        expect(find_field('creature[description]').value).to eq(creature.description)
+        expect(find_field('creature[image_url]').value).to eq(creature.image_url)
+        expect(find_field('creature_status_active').checked?).to be(true)
+        expect(find_field('creature_status_retired').checked?).to be(false)
       end
 
-      fill_in creature[breed], with: 'Whatisthis'
-      fill_in creature[description], with: 'Very angry animal'
-      fill_in creature[image_url], with: 'http://bit.ly/2oiL0yl'
-      select 'Retired', from: 'status-box'
+      fill_in 'creature[breed]', with: 'Whatisthis'
+      fill_in 'creature[description]', with: 'Very angry animal'
+      fill_in 'creature[image_url]', with: 'http://bit.ly/2oiL0yl'
+      choose 'creature_status_retired'
 
-      within('.creature') do
-        expect(page).to have_content('Name: Whatisthis')
+      click_on 'Update Creature'
+
+      within("#creature-#{creature.id}") do
+        expect(page).to have_content('Breed: Whatisthis')
         expect(page).to have_content('Description: Very angry animal')
-        expect(page).to have_css('img[src=http://bit.ly/2oiL0yl]')
+        expect(page).to have_css("img[src='http://bit.ly/2oiL0yl']")
         expect(page).to have_content('Status: Retired')
       end
     end
