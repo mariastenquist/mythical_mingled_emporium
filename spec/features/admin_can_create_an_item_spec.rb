338803e3425fc065fc14aac69287add3896727ec
mariastenquist <stenquistm@gmail.com>
2017-04-18 16:36:01 -0600

    add functionality to admin creature show

--- spec/features/admin_can_create_an_item_spec.rb
+++ spec/features/admin_can_create_an_item_spec.rb
@@ -1,30 +1,40 @@
 require 'rails_helper'
 
 RSpec.feature 'Admin can create an item' do
-  scenario 'admin creates an item' do
-    create :user, role: 1
-    create :category, name: 'wtf'
+  context 'all input provided' do 
+    scenario 'admin creates an item' do
+      create :user, role: 1
+      create :category, name: 'wtf'
 
-    visit new_admin_creature_path
+      visit new_admin_creature_path
 
-    fill_in 'creature[breed]', with: 'PigHuman'
-    fill_in 'creature[price]', with: '100.00'
-    fill_in 'creature[description]', with: 'Oh god what have we done?'
-    select 'wtf', from: 'creature[categories]'
+      fill_in 'creature[breed]', with: 'PigHuman'
+      fill_in 'creature[price]', with: '100.00'
+      fill_in 'creature[description]', with: 'Oh god what have we done?'
+      fill_in 'creature[image_url]', with: 'http://bit.ly/2pQ0Oc9'
+      select 'wtf', from: 'creature[categories]'
 
-    # from: http://www.charliemassry.com/posts/29-testing-paperclip-with-rspec-capybara-and-factory-girl
-    click_on 'Add photo'
-    attach_file 'File', 'spec/asset_specs/photo.jpg'
-    click_on 'Add photo'
+      # click_on 'Add photo'
+      # attach_file 'File', 'spec/asset_specs/photo.jpg'
+      # click_on 'Add photo'
 
-    click_on 'Create'
+      click_on 'Create'
 
-    expect(current_path).to eq creature_path(Creature.first.id)
+      expect(current_path).to eq admin_creature_path(Creature.first.id)
 
-    expect(page).to have_content 'PigHuman'
-    expect(page).to have_content 'Price: $100.00'
-    expect(page).to have_content 'Description: Oh god what have we done?'
-    expect(page).to have_content 'Description: Oh god what have we done?'
-    expect(page).to have_selector 'img'
+      expect(page).to have_content 'PigHuman'
+      expect(page).to have_content 'Price: $100.00'
+      expect(page).to have_content 'Description: Oh god what have we done?'
+      expect(page).to have_content 'Description: Oh god what have we done?'
+      expect(page).to have_selector 'img'
+    end
+  end
+
+  context 'admin provides incomplete information' do
+
+  end
+
+  context 'admin is able to add a photo or have default' do
+
   end
 end
