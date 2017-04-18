f1fc2dd03d28bd43a476fa723d2fc83264d5ce7f
dannyradden <dradden@gmail.com>
2017-04-18 10:01:03 -0600

    Add admin can create new creature
    add new creature feature test
    Add admin ability to create a creature
    add functionality to admin creature show
    Add sad path for admin creating a creature
    squashing hound

---
+++ app/controllers/admin/creatures_controller.rb
@@ -0,0 +1,30 @@
+class Admin::CreaturesController < ApplicationController
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    @creature = Creature.new(creature_params)
+    if @creature.save
+      flash[:success] = 'Creature successfully created!'
+      redirect_to admin_creature_path(@creature)
+    else
+      flash[:failure] = 'Retry creature creation'
+      render :new
+    end
+  end
+
+  def show
+    @creature = Creature.find(params[:id])
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:breed,
+                                     :description,
+                                     :price,
+                                     :image_url,
+                                     :category_id)
+  end
+end
