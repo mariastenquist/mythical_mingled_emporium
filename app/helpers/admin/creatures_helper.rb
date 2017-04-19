086ee116e5eb2e2caf98d7e047d64980a2a9de57
Sam Landfried <samlandfried@gmail.com>
2017-04-19 11:17:26 -0600

    Refactor admin creatures

--- app/helpers/admin/creatures_helper.rb
+++ app/helpers/admin/creatures_helper.rb
@@ -1,2 +1,39 @@
 module Admin::CreaturesHelper
+  def orders_count_link
+    link_to "All: #{@all_orders.count}", admin_orders_path
+  end
+
+  def ordered_count_link
+    link_to "Ordered: #{@all_orders.ordered.count}", admin_orders_path(filter: 'ordered')
+  end
+
+  def paid_count_link
+    link_to "Paid: #{@all_orders.paid.count}", admin_orders_path(filter: 'paid')
+  end
+
+  def completed_count_link
+    link_to "Completed: #{@all_orders.completed.count}", admin_orders_path(filter: 'completed')
+  end
+
+  def cancelled_count_link
+    link_to "Cancelled: #{@all_orders.cancelled.count}", admin_orders_path(filter: 'cancelled')
+  end
+
+  def mark_as_paid_link
+    return unless @order.ordered?
+
+    link_to 'Mark as paid', admin_order_path(@order, status: 'paid'), method: :put
+  end
+
+  def cancel_link
+    return unless @order.paid? || @order.ordered?
+
+    link_to 'Cancel', order_path(@order), method: :put
+  end
+
+  def mark_as_completed_link
+    return unless @order.paid?
+
+    link_to 'Mark as completed', admin_order_path(@order, status: 'completed'), method: :put
+  end
 end
